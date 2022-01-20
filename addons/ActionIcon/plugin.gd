089e5e15f4eb0ea3de5110b97e32ab2c63f203b0
kobewi <kobewi4e@gmail.com>
2022-01-20 23:07:38 +0100

    Cleanup after previous commit

--- addons/ActionIcon/plugin.gd
+++ addons/ActionIcon/plugin.gd
@@ -1,10 +1,8 @@
 tool
 extends EditorPlugin
 
-
 func _enter_tree():
 	add_custom_type("ActionIcon", "TextureRect", preload("ActionIcon.gd"), preload("icon.png"))
-
 
 func _exit_tree():
 	remove_custom_type("ActionIcon")
